8de8af055335f71fd7cfcfb9e6722f90d4745fb0
Will Taylor <41151984+witaylor@users.noreply.github.com>
2023-05-25 14:41:21 +0100

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import * as path from "path";
+import cors from "cors";
 
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
 
@@ -12,6 +13,7 @@ export const app = express();
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 
+app.use(cors());
 app.use(logger("dev"));
 
 app.use(express.static(path.join(__dirname, "../public")));
